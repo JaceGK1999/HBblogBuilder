e4b8fa48dfe3935a7d69c12074c5def6e773ee8d
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-09 16:45:03 -0800

    subtitles now working

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,16 +7,19 @@ import './Home.css';
 
 export default function Home() {
   const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
   // add useState calls here for title, subtitle, font, align, and text
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview {...{ title }} />
+      <Preview {...{ title, subtitle }} />
       <Editor
         {...{
           title,
           setTitle,
+          subtitle,
+          setSubtitle,
         }}
       />
     </main>
